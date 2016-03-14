075a5030368203edac6858c3b2d86572ee356c93
Minwoo Lee <ermaker@gmail.com>
2016-03-14 14:03:06 +0900

    Update docker_env.sh

--- docker_env.sh
+++ docker_env.sh
@@ -5,5 +5,5 @@ export http_proxy=http://168.219.61.252:8080/
 export ftp_proxy=ftp://168.219.61.252:8080/
 export https_proxy=http://168.219.61.252:8080/
 export no_proxy=qb.sec.samsung.net,165.213.180.100,127.0.0.1,localhost
-export SSL_CERT_FILE=/usr/share/ca-certificates/samsung.crt
+export SSL_CERT_FILE=/usr/share/ca-certificates/infra.crt
 EOS
